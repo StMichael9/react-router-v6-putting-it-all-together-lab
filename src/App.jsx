e210a59f7c43026899180104b54077f7e2c27680
St Michael Egenamba <michaelegenamba@gmail.com>
2026-05-14 09:51:49 -0700

    Learned find to finish DirectorCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-const App = () => {
-    return (
-        <>
-        {/* define all routes here */}
-        </>
-    )
-}
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import DirectorContainer from "./pages/DirectorContainer";
+import DirectorList from "./pages/DirectorList";
+import DirectorForm from "./pages/DirectorForm";
+import DirectorCard from "./pages/DirectorCard";
+import MovieForm from "./pages/MovieForm";
+import MovieCard from "./pages/MovieCard";
 
-export default App
+const App = () => {
+  return (
+    <>
+      {/* define all routes here */}
+      <BrowserRouter>
+        <Routes>
+          {/* Top level routes*/}
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          {/* DIRECTORS PARENT ROUTE */}
+          <Route path="directors" element={<DirectorContainer />}>
+            {/* NESTED UNDER /directors */}
+            <Route index element={<DirectorList />} />
+            <Route path="new" element={<DirectorForm />} />
+            <Route path=":id" element={<DirectorCard />}>
+              <Route path="movies">
+                <Route path="new" element={<MovieForm />} />
+                <Route path=":movieId" element={<MovieCard />} />
+              </Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
--- src/pages/DirectorCard.jsx
+++ src/pages/DirectorCard.jsx
@@ -1,28 +1,31 @@
+import { useOutletContext, Link, useParams, Outlet } from "react-router-dom";
 
 function DirectorCard() {
-    // Replace me
-    const director = null
+  const { directors, setDirectors } = useOutletContext();
+  const { id } = useParams();
+  // Used find method instead of For of loop
+  const director = directors.find((d) => String(d.id) === String(id));
 
-    if (!director) {
-        return <h2>Director not found.</h2>
-    }
+  if (!director) {
+    return <h2>Director not found.</h2>;
+  }
 
-    return (
-        <div>
-        <h2>{director.name}</h2>
-        <p>{director.bio}</p>
-        <h3>Movies:</h3>
-        <ul>
-            {director.movies.map((movie) => (
-            <li key={movie.id}>
-                <a>{movie.title}</a>
-            </li>
-            ))}
-        </ul>
-        <Link to={`movies/new`}>Add New Movie</Link>
-        {/* Movie compoenents should render here depending on route */}
-        </div>
-    )
+  return (
+    <div>
+      <h2>{director.name}</h2>
+      <p>{director.bio}</p>
+      <h3>Movies:</h3>
+      <ul>
+        {director.movies.map((movie) => (
+          <li key={movie.id}>
+            <Link to={`movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
+      </ul>
+      <Link to={`movies/new`}>Add New Movie</Link>
+      {/* Movie compoenents should render here depending on route */}
+    </div>
+  );
 }
 
-export default DirectorCard
+export default DirectorCard;
